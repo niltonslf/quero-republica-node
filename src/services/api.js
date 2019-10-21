bee324094806258eaa54ef0252665255e38fbf0b
Nilton Lopes <niltonrck@gmail.com>
2019-10-21 00:03:00 -0300

    INT: Altera url para servidor

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const api = axios.create({
-  baseURL: 'http://localhost:8001/api'
+  baseURL: 'http://webapp152181.ip-45-33-7-164.cloudezapp.io:8001/api'
 })
 
 module.exports = api
