44b3d65a64b7452770e56876bfeeed1759b3dbbb
Nilton Lopes <niltonrck@gmail.com>
2019-10-21 15:38:23 -0300

    FIX: altera url do servidor pra aws

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const api = axios.create({
-  baseURL: 'http://webapp152181.ip-45-33-7-164.cloudezapp.io:8001/api'
+  baseURL: 'http://ec2-18-228-154-218.sa-east-1.compute.amazonaws.com:8001/api'
 })
 
 module.exports = api
